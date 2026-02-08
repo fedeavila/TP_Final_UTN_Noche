d447aa2cb848b4447defc80023abb96367285c68
Federico Avila <fedeavila@outlook.com>
2026-02-08 17:18:31 -0300

    De ahora en adelante, cada vez que se requiera hacer una consulta, actualización, alta o eliminación, se deberá realizar el registro de usuario y posterior login.

--- src/app.js
+++ src/app.js
@@ -6,15 +6,20 @@ import chatsRoutes from "./routes/chats.routes.js";
 import messagesRoutes from "./routes/messages.routes.js";
 import usersRoutes from "./routes/users.routes.js";
 
+import authRoutes from './routes/authRoutes.js';
+
 dotenv.config();
 connectDB();
 
 const app = express();
+
 app.use(express.json());
 
 app.use("/users", usersRoutes);
 app.use("/chats", chatsRoutes);
 app.use("/messages", messagesRoutes);
+app.use('/auth', authRoutes);
+
 
 app.use((err, req, res, next) => {
     res.status(500).json({ success: false, message: err.message });
@@ -22,3 +27,5 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`SERVIDOR CORRIENDO EN PUERTO: ${PORT}`));
+
+export default app;
